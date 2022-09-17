3ca5ec9f5aa6db2f24141c7683d74635c8deb7b9
Betelhem-hailu <betelhemhailu811@gmail.com>
2022-09-17 04:38:53 +0300

    puppet install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install framework using Puppet
-file { 'flask':
-ensure => '2.1.0',
-validate_cmd => 'pip3 install flask',
-}
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem;
+  }
